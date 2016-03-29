8048d5de6c664e0e7f98935fc2699cf8635c13bc
justinrjoseph <justin.joseph0@yahoo.com>
2016-03-29 22:04:06 +0000

    Allow venching machine to receive nickels

--- vending-machine.rb
+++ vending-machine.rb
@@ -2,6 +2,18 @@ require 'minitest/autorun'
 
 class VendingMachine
   
+  def receive_coin(coin)
+    @running_total = 0
+    
+    case coin
+    when :n, :nickel
+      @running_total += 0.05
+    end
+  end
+  
+  def running_total
+    @running_total
+  end
   
 end
 
@@ -9,8 +21,12 @@ end
 
 class VendingMachineTest < MiniTest::Test
   
-  def test_vending_machine_instantiation
+  def test_vending_machine_accepts_nickels
     @vm = VendingMachine.new
+    
+    @vm.receive_coin :nickel
+    
+    assert_equal 0.05, @vm.running_total, "Vending machine does not accept nickels?"
   end
   
 end
