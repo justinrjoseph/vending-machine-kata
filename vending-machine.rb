e31525255f64d266c10e9df5d114ccfbca22e8d3
justinrjoseph <justin.joseph0@yahoo.com>
2016-03-29 01:46:15 +0000

    Initial commit

---
+++ vending-machine.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+
+class VendingMachine
+  
+  
+end
+
+
+
+class VendingMachineTest < MiniTest::Test
+  
+  def test_vending_machine_instantiation
+    @vm = VendingMachine.new
+  end
+  
+end
